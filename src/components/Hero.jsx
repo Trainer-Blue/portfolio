77accba3f37d8b2850cc0c3274ae1b392b6fa477
Ishan Siddhartha <ishansiddhartha@gmail.com>
2025-03-11 14:19:54 +0530

    backdrop blur to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,15 +4,15 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="hero-container">
-        <div className="hero-content">
+        <div className="hero-content backdrop-blur-[2px] rounded-2xl p-8 md:p-12 shadow-xl">
           <h1 className="hero-title">
             <span className="animate-hello">Hello 👋🏽</span><br />
             <p className="hero-subtitle animate-iam">I am </p>
             <span className="gradient-text animate-name">Ishan </span>
             <span className="animate-name-2">Siddhartha</span>
           </h1>
+          <div className="hero-bg-text animate-dev"><p>DEVELOPER</p></div>
         </div>
-        <div className="hero-bg-text animate-dev"><p>DEVELOPER</p></div>
     </div>
   );
 };
